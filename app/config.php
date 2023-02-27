602eca2e7f965c9efd7254a60d7c69680a9f13da
home <home@pop-os.localdomain>
2023-02-28 01:23:50 +0300

    prepublic

--- app/config.php
+++ app/config.php
@@ -7,7 +7,7 @@ $clientId = 'efeb1bfa-22a0-4a23-8505-84853b3d55e5';
 // Secret key: в настройках виджета
 $clientSecret = 'f1Yo5FoWo82YxLUkWCKpvnWdygexMO3bmVf3nPN7rClfw2RlMZp74WoCKAeTq9HM';
 // $redirectUri = 'https://b5bd-85-249-38-193.eu.ngrok.io/amocrm/get_token.php';
-$redirectUri = 'https://b5bd-85-249-38-193.eu.ngrok.io';
+$redirectUri = 'http://emfy.ct96865.tw1.ru';
 
 $baseDomain = 'https://emfytest01.kommo.com/'; // или yoursubdomain.amocrm.ru - ваша страница
 // $my_secret для проверки доступа, такой же в script.js
